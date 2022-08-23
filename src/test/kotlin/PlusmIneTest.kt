e1e571caa99b670eea8d77599cc25f081dbb5872
murillo <tumusx1@gmail.com>
2022-08-23 12:53:44 -0300

    criado testes

--- src/test/kotlin/PlusmIneTest.kt
+++ src/test/kotlin/PlusmIneTest.kt
@@ -1,9 +1,19 @@
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContains
 
 class PlusmIneTest {
+    val userListInterface = mutableListOf("José", "Raimundo", "Mamacita")
 
+    //give
     @Test
-    fun whenPlusMineNumbers() {
+    fun insertUserInList() {
+        //when
+        userListInterface.add("Murillo")
     }
 
+    @Test
+    fun searchNameUser() {
+        assertContains(userListInterface, "Murillo")
+    }
 }
